ab4f59f5f3066f84dbff5037eaec86a8273a9e44
emanuelfeld <emanuelfeld@gmail.com>
2017-12-17 00:54:59 -0500

    day 17 fix

--- day_17/main.py
+++ day_17/main.py
@@ -6,12 +6,12 @@ def part_1(steps, max_val):
     index = 0
     value = 0
 
-    while value < max_val + 1:
+    while value < max_val:
         value += 1
         index = (index + steps) % value + 1
         buff.insert(index, value)
 
-    return buff[buff.index(max_val) + 1]
+    return buff[index + 1]
 
 print('part 1:', part_1(steps, 2017))
 
@@ -21,13 +21,12 @@ def part_2(steps, max_val):
     value = 0
     answer = None
 
-    while value < max_val + 1:
+    while value < max_val:
         value += 1
         index = (index + steps) % value + 1
         if index == 1:
             answer = value
 
     return answer
-
 
 print('part 2:', part_2(steps, 50000000))
